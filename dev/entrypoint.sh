85c1820fd57b9651e6951bc27d01cbad34ad1f8a
Naoto Kamba <ayataka.nk@gmail.com>
2020-06-09 16:22:09 +0900

    entrypoint.shの内容をdocker-compose内に記述

--- dev/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir /root/.ssh
-chmod 700 /root/.ssh
-cp -R /optionfiles/id_rsa /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-git config --global user.email "$GIT_USER_EMAIL"
-git config --global user.name "$GIT_USER_NAME"
-/bin/bash
